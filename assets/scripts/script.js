c2bf2384e3d60bbd4e1645be77713e473952590f
AAmanzi <alnick.amanzi@gmail.com>
2019-03-14 22:51:58 +0100

    Replaced css hover events with js variant

---
+++ assets/scripts/script.js
@@ -0,0 +1,112 @@
+let dropdownList = document.querySelectorAll(".dropdown__list");
+    
+function AddFilter(dropdown, filter){
+    filter.Code = 
+    `<li class="dropdown__list__item">
+        ${filter.Type}
+        <span class="list__item__count">
+            (${filter.Count})
+        </span>
+    </li>`;
+
+    dropdown.innerHTML += filter.Code;
+}
+
+dropdownList.forEach(element => {
+    AddFilter(element, {Type: "Magenta ", Count: 1032});
+    AddFilter(element, {Type: "Fuscia ", Count: 1032});
+    AddFilter(element, {Type: "Burgundy ", Count: 1032});
+});
+
+let offers = [
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: "Wrangler small logo crew neck t-shirt in white",
+        Price: "£16.63"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: "Wrangler logo chest stripe rugby polo in blue/white",
+        Price: "£50.63"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: "Wrangler kobel retro large logo ringer t-shirt in white",
+        Price: "£20.97"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: "Lyle & Scott polo in burgundy",
+        Price: "£44.12"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: `<span class="item__paragraph__brand">COLLUSION</span> Unisex 
+            long sleeve t-shirt with graphic print in neon green`,
+        Price: "£12.29"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: `<span class="item__paragraph__brand">ASOS DESIGN</span> Disney 
+            oversized t-shirt with rainbow`,
+        Price: "£22.42"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: `<span class="item__paragraph__brand">COLLUSION</span> Unisex 
+            oversized t-shirt with back print`,
+        Price: "£10.12"
+    },
+    {
+        Image: "assets/images/offer.jpg",
+        Description: "TALL",
+        Paragraph: `<span class="item__paragraph__brand">ASOS DESIGN</span> Mickey 
+            reloxed t-shirt with retro print`,
+        Price: "£18.08"
+    }
+]
+
+function AddOfferItems(location, offersList){
+    offersList.forEach(offer => {
+        offer.Code = `<div class="offers__item">
+        <img class="item__img" src="${offer.Image}" alt="Offer" />
+        <img class="item__heart-shape" src="./assets/images/heart-shape.png" alt="Hearth Shape">
+        <img class="item__heart-full" src="./assets/images/heart-full.png" alt="Hearth Full">
+        <p class="item__img-description">${offer.Description}</p>
+        <p class="item__paragraph">
+            ${offer.Paragraph}
+        </p>
+        <span class="item__price">${offer.Price}</span>
+      </div>`;
+
+      location.innerHTML += offer.Code;
+    });
+}
+
+AddOfferItems(document.querySelector(".main__offers"), offers);
+
+let offerItems = document.querySelectorAll(".offers__item");
+
+offerItems.forEach(offerItem => {
+    offerItem.addEventListener("mouseover", function() {
+        let itemDescription = offerItem.querySelector(".item__img-description");
+        itemDescription.classList.add("item__img-description-hover");
+    });
+
+    offerItem.addEventListener("mouseout", function() {
+        let itemDescription = offerItem.querySelector(".item__img-description");
+        itemDescription.classList.remove("item__img-description-hover");
+    });
+    
+    let itemHeart = offerItem.querySelector(".item__heart-full");
+    itemHeart.addEventListener("click", function() {
+        itemHeart.classList.toggle("item__heart-full-hover");
+    });
+})
