a896c3c04b1adb9ded680d117ea9cbcfa09ae871
AAmanzi <alnick.amanzi@gmail.com>
2019-03-15 21:19:26 +0100

    Rough finish

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,52 +21,52 @@ dropdownList.forEach(element => {
 let offers = [
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: "Wrangler small logo crew neck t-shirt in white",
         Price: "£16.63"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: "Wrangler logo chest stripe rugby polo in blue/white",
         Price: "£50.63"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: "Wrangler kobel retro large logo ringer t-shirt in white",
         Price: "£20.97"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: "Lyle & Scott polo in burgundy",
         Price: "£44.12"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: `<span class="item__paragraph__brand">COLLUSION</span> Unisex 
             long sleeve t-shirt with graphic print in neon green`,
         Price: "£12.29"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: `<span class="item__paragraph__brand">ASOS DESIGN</span> Disney 
             oversized t-shirt with rainbow`,
         Price: "£22.42"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: `<span class="item__paragraph__brand">COLLUSION</span> Unisex 
             oversized t-shirt with back print`,
         Price: "£10.12"
     },
     {
         Image: "assets/images/offer.jpg",
-        Description: "TALL",
+        Description: "SEXY BOI",
         Paragraph: `<span class="item__paragraph__brand">ASOS DESIGN</span> Mickey 
             reloxed t-shirt with retro print`,
         Price: "£18.08"
@@ -92,12 +92,16 @@ function AddOfferItems(location, offersList){
 
 AddOfferItems(document.querySelector(".main__offers"), offers);
 
+let mainFavourites = document.querySelector(".main__favourites-found");
 let offerItems = document.querySelectorAll(".offers__item");
+let favourites = 0;
+let numberPattern = /\d+/;
 
 offerItems.forEach(offerItem => {
     offerItem.addEventListener("mouseover", function() {
         let itemDescription = offerItem.querySelector(".item__img-description");
         itemDescription.classList.add("item__img-description-hover");
+        itemHeart.isTriggered = false;
     });
 
     offerItem.addEventListener("mouseout", function() {
@@ -106,7 +110,36 @@ offerItems.forEach(offerItem => {
     });
     
     let itemHeart = offerItem.querySelector(".item__heart-full");
+
     itemHeart.addEventListener("click", function() {
-        itemHeart.classList.toggle("item__heart-full-hover");
+        itemHeart.classList.toggle("item__heart-full-click");
+
+        if(itemHeart.classList.contains("item__heart-full-click")){
+            favourites++;
+        }
+        else{
+            favourites--;
+        }
+
+        mainFavourites.innerHTML = `Favourites: ${favourites}`;
+
+        itemHeart.isTriggered = true;
+    });
+
+
+    offerItem.addEventListener("click", function() {
+        if(itemHeart.isTriggered){
+            return null;
+        }
+
+        let offerWindow = window.open("", "_blank");
+        offerWindow.document.write(`<link rel="stylesheet" href="./assets/styles/main.css"/>
+        <link rel="stylesheet" href="./assets/styles/common.css" />
+        <main>
+            <div class="main__offers">
+        ${offerItem.outerHTML}
+            </div>
+        </main>`);
     });
 })
+
